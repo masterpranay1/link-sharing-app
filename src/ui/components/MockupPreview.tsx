17ee0ec8759fffecfdf5187927fb2427e76f7825
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-16 22:32:05 +0530

    Feat : MockupPreview Button Component

--- src/ui/components/MockupPreview.tsx
+++ src/ui/components/MockupPreview.tsx
@@ -1,5 +1,31 @@
 import RightArrow from "../../assets/right-arrow.png";
 
+const Button = ({
+  text,
+  bgcolor,
+  textColor = "text-white",
+}: {
+  text: string;
+  bgcolor: string;
+  textColor?: string;
+}) => {
+  return (
+    <button
+      className={`flex flex-row gap-2 items-center justify-center ${bgcolor} ${textColor} px-4 py-2 rounded-lg`}
+    >
+      {/* <span>Image</span> */}
+      <div className="block">{text}</div>
+      <img
+        width="24"
+        height="32"
+        src={RightArrow}
+        alt="long-arrow-right"
+        className="ml-auto"
+      />
+    </button>
+  );
+};
+
 export default function MockupPreview({ className }: { className?: string }) {
   return (
     <div className={`${className} bg-white rounded-lg p-4 lg:p-12 lg:px-16`}>
@@ -24,41 +50,9 @@ export default function MockupPreview({ className }: { className?: string }) {
             </p>
 
             <div className="button-wrapper mx-auto w-full flex flex-col gap-4 px-4 py-4 mt-8">
-              <button className="flex flex-row gap-2 items-center justify-center bg-slate-800 text-white px-4 py-2 rounded-lg">
-                {/* <span>Image</span> */}
-                <div className="block">Github</div>
-                <img
-                  width="24"
-                  height="32"
-                  src={RightArrow}
-                  alt="long-arrow-right"
-                  className="ml-auto"
-                />
-              </button>
-
-              <button className="flex flex-row gap-2 items-center justify-center bg-red-600 text-white px-4 py-2 rounded-lg">
-                {/* <span>Image</span> */}
-                <div className="block">Google</div>
-                <img
-                  width="24"
-                  height="32"
-                  src={RightArrow}
-                  alt="long-arrow-right"
-                  className="ml-auto"
-                />
-              </button>
-
-              <button className="flex flex-row gap-2 items-center justify-center bg-blue-600 text-white px-4 py-2 rounded-lg">
-                {/* <span>Image</span> */}
-                <div className="block">Linkedin</div>
-                <img
-                  width="24"
-                  height="32"
-                  src={RightArrow}
-                  alt="long-arrow-right"
-                  className="ml-auto"
-                />
-              </button>
+              <Button text="Github" bgcolor="bg-slate-800" />
+              <Button text="Google" bgcolor="bg-red-600" />
+              <Button text="Linkedin" bgcolor="bg-blue-600" />
             </div>
           </div>
         </div>
