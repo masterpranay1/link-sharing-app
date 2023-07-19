bd7d6f3567cf25146554f49dce3e297bf15047a1
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-20 02:59:08 +0530

    Feat : Global Context added to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.tsx";
+import { GlobalProvider } from "./services/context/index.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
