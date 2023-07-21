d0842cf4013ff46fa54e6f27cdbf862f1fda5fad
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-21 13:01:39 +0530

    Refactor : user adapter updated

--- src/services/userAdapter.ts
+++ src/services/userAdapter.ts
@@ -1,29 +1,34 @@
-import  { account } from "../lib/appwrite";
-import { v4 as uuidv4 } from 'uuid';
+import { account } from "../lib/appwrite";
+import { v4 as uuidv4 } from "uuid";
 import { ICreateUser, IEmailLoginUser } from "../application/ports";
+import { notifyError, notifySuccess } from "./notification";
 
-export const useCreateUserService = () : ICreateUser => {
+export const useCreateUserService = (): ICreateUser => {
   const createUser = async (email: string, password: string) => {
     try {
-      const res = await account.create(uuidv4(),email, password);
-      console.log(res)
-    } catch (error) {
-      console.error(error);
+      const res = await account.create(uuidv4(), email, password);
+      notifySuccess("User created successfully");
+      return res.$id;
+    } catch (error: any) {
+      notifyError(error.message);
+      return null;
     }
   };
 
   return { createUser };
-}
+};
 
-export const useEmailLoginUserService = () : IEmailLoginUser => {
+export const useEmailLoginUserService = (): IEmailLoginUser => {
   const emailLoginUser = async (email: string, password: string) => {
     try {
-      const res = await account.createEmailSession(email, password)
-      console.log(res)
-    } catch(error) {
-      console.log(error)
+      const res = await account.createEmailSession(email, password);
+      notifySuccess("Login successful");
+      return res.$id;
+    } catch (error: any) {
+      notifyError(error.message);
+      return null;
     }
-  }
+  };
 
-  return { emailLoginUser }
-}
+  return { emailLoginUser };
+};
