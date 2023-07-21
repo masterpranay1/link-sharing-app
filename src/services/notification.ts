8bc7a83fd9a9d529464fac1f0866ad95afdbebc7
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-21 13:01:29 +0530

    Feat : Notification service

---
+++ src/services/notification.ts
@@ -0,0 +1,26 @@
+import { toast } from "react-toastify";
+
+export const notifySuccess = (message: string) => {
+  return toast.success(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
+
+export const notifyError = (message: string) => {
+  return toast.error(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "light",
+  });
+}
