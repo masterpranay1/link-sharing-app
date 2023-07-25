56b21e1d46e9dcf112b7c8549a3d224f61656f91
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-25 19:00:10 +0530

    Feat : Ports - ILogoutUser, IGetUserInfo, ISaveUserInfo

--- src/application/ports/index.ts
+++ src/application/ports/index.ts
@@ -1,7 +1,21 @@
+import { UserInfo } from '@/domain/user'
+
 export interface ICreateUser {
   createUser : (email : string, password: string) => Promise<string | null>
 }
 
 export interface IEmailLoginUser {
   emailLoginUser : (email : string, password : string) => Promise<string | null>
+}
+
+export interface ILogoutUser {
+  logoutUser : () => Promise<void>
+}
+
+export interface IGetUserInfo {
+  getUserInfo : () => Promise<UserInfo | null>
+}
+
+export interface ISaveUserInfo {
+  saveUserInfo : (userInfo : UserInfo) => Promise<boolean>
 }
