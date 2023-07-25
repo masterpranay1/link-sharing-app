931b30911185ac5a16338d56934d1c4581e3769b
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-25 19:01:23 +0530

    Refacor : Appwrite - added databases and query

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { Account, Client } from "appwrite";
+import { Account, Client, Databases, Query } from "appwrite";
 
 const client = new Client();
 
@@ -8,3 +8,5 @@ client
   .setProject("64b4602b93dd06aa7d86");
 
 export const account = new Account(client);
+export const databases = new Databases(client);
+export const query = Query
