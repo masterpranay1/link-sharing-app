109b7aaa4b722af1e43f075ff8707c04856587dd
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-25 18:59:09 +0530

    Feat : User Domain ( UserInfo )

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -2,3 +2,12 @@ export interface User {
   email : string;
   password : string;
 }
+
+export interface UserInfo {
+  userid: string;
+  firstname?: string;
+  lastname?: string;
+  profileImage?: string;
+  links?: string[];
+  email?: string;
+}
