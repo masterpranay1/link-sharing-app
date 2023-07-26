3c8bd5a64d49d47dac377a6b0b4965d560fb7249
masterpranay1 <rajpranaychottu@gmail.com>
2023-07-26 23:54:23 +0530

    Refactor : Preview.tsx Component

--- src/ui/page/Preview.tsx
+++ src/ui/page/Preview.tsx
@@ -8,13 +8,19 @@ import { useGlobal } from "@/services/context";
 import LinkedinIcon from "@/assets/linkedin-icon.png";
 import TwitterIcon from "@/assets/twitter-icon.png";
 import GithubIcon from "@/assets/github-icon.png";
-import { useEffect } from "react";
+
+import LinkIcon from "@/assets/link.png";
+
+import { useGetUserInfo } from "@/application/useUserInfo";
+import { useGetLinks } from "@/application/useLink";
+import { useEffect, useState } from "react";
 
 interface IButtonProps {
   text: string;
   bgcolor: string;
   textColor?: string;
   imageUrl?: string;
+  url?: string;
 }
 
 const Button = ({
@@ -22,9 +28,12 @@ const Button = ({
   bgcolor,
   textColor = "text-white",
   imageUrl,
+  url,
 }: IButtonProps) => {
   return (
-    <button
+    <a
+      href={url}
+      target="_blank"
       className={`flex flex-row gap-4 items-center justify-center ${bgcolor} ${textColor} px-6 py-4 rounded-lg`}
     >
       <img
@@ -39,7 +48,7 @@ const Button = ({
         alt="long-arrow-right"
         className="ml-auto"
       />
-    </button>
+    </a>
   );
 };
 
@@ -47,6 +56,68 @@ export default function Preview() {
   const { navState, dispatchNavLink, userState } = useGlobal();
   const { pathname } = useLocation();
   const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    profilepicture: "https://via.placeholder.com/250",
+  });
+
+  const [links, setLinks] = useState<
+    {
+      id: string;
+      platform: string;
+      url: string;
+    }[]
+  >([]);
+
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [bgColors, setBgColors] = useState<string[]>([]);
+
+  const getUserInfo = useGetUserInfo();
+  const { getLinksHandler } = useGetLinks();
+
+  useEffect(() => {
+    getUserInfo().then((data) => {
+      if (!data) return;
+      setUserInfo({
+        firstname: data.firstname as string,
+        lastname: data.lastname as string,
+        email: data.email as string,
+        profilepicture: data.profileImage as string,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    getLinksHandler().then((data) => {
+      if (!data) return;
+      setLinks(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setImageUrls(
+      links.map((link) => {
+        if (link.platform === "Github") return GithubIcon as string;
+        if (link.platform === "Twitter") return TwitterIcon as string;
+        if (link.platform === "Linkedin") return LinkedinIcon as string;
+
+        return LinkIcon as string;
+      })
+    );
+
+    setBgColors(
+      links.map((link) => {
+        if (link.platform === "Github") return "bg-slate-800";
+        if (link.platform === "Twitter") return "bg-blue-400";
+        if (link.platform === "Linkedin") return "bg-blue-600";
+
+        return "bg-slate-600";
+      })
+    );
+  }, [links]);
 
   const handleClick = () => {
     dispatchNavLink(navState.previousId);
@@ -56,7 +127,7 @@ export default function Preview() {
     if (pathname.slice(1) !== navState.id) {
       dispatchNavLink("preview");
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (userState.id === null) {
@@ -94,35 +165,31 @@ export default function Preview() {
           <div className="w-full md:w-96 mx-auto my-4 -translate-y-12 md:-translate-y-24 bg-white shadow-lg px-4 py-8 rounded-lg m-4">
             <figure>
               <img
-                src={DPImage}
+                src={userInfo.profilepicture}
                 alt="profile"
                 className="w-24 h-24 lg:w-32 lg:h-32 mx-auto rounded-full border-2 border-slate-400"
               />
             </figure>
 
-            <h2 className="text-lg text-center font-bold my-2 text-slate-600">
-              Pranay Raj
-            </h2>
+            {userInfo.firstname && userInfo.lastname && (
+              <h2 className="text-lg text-center font-bold my-2 text-slate-600">
+                {userInfo.firstname} {userInfo.lastname}
+              </h2>
+            )}
             <p className="text-md text-center break-all mx-2 text-slate-600">
-              masterpranayraj@gmail.com
+              {userInfo.email}
             </p>
 
             <div className="button-wrapper mx-auto w-full flex flex-col gap-4 px-4 py-4 mt-8">
-              <Button
-                text="Github"
-                bgcolor="bg-slate-800"
-                imageUrl={GithubIcon}
-              />
-              <Button
-                text="Twitter"
-                bgcolor="bg-blue-400"
-                imageUrl={TwitterIcon}
-              />
-              <Button
-                text="Linkedin"
-                bgcolor="bg-blue-600"
-                imageUrl={LinkedinIcon}
-              />
+              {links.map((link) => (
+                <Button
+                  key={link.id}
+                  text={link.platform}
+                  url={link.url}
+                  bgcolor={bgColors[links.indexOf(link)]}
+                  imageUrl={imageUrls[links.indexOf(link)]}
+                />
+              ))}
             </div>
           </div>
         </div>
